1bd581b5374916d51635c52399f40d7a5030dc00
ssy523a <ssy523a@gmail.com>
2026-08-08 11:35:43 +0900

    Initial Commit

---
+++ AirQuality5/AirQuality5App.swift
@@ -0,0 +1,17 @@
+//
+//  AirQuality5App.swift
+//  AirQuality5
+//
+//  Created by Seyoung Seo on 8/8/26.
+//
+
+import SwiftUI
+
+@main
+struct AirQuality5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
